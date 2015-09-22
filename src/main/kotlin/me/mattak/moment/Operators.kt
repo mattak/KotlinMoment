9a8cb4e10f4482b79a004c0c177b99c5fbfd5250
mattak <mattak.me@gmail.com>
2015-09-23 01:24:32 +0900

    feat(Operator): add full operator

--- src/main/kotlin/me/mattak/moment/Operators.kt
+++ src/main/kotlin/me/mattak/moment/Operators.kt
@@ -3,3 +3,27 @@ package me.mattak.moment
 fun Moment.minus(rhs: Moment): Duration {
     return this.intervalSince(rhs)
 }
+
+fun Moment.plus(rhs: Duration): Moment {
+    return this.add(rhs)
+}
+
+fun Duration.plus(rhs: Moment): Moment {
+    return rhs.add(this)
+}
+
+fun Moment.minus(rhs: Duration): Moment {
+    return this.subtract(rhs)
+}
+
+fun Duration.minus(rhs: Moment): Moment {
+    return rhs.subtract(this)
+}
+
+fun Duration.plus(rhs: Duration): Duration {
+    return rhs.add(this)
+}
+
+fun Duration.minus(rhs: Duration): Duration {
+    return rhs.subtract(this)
+}
